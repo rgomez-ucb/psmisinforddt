3a225d85dbe2046968f61fa13e7ea5917b006150
llllllllllIIIIl <jiyangli123@gmail.com>
2025-11-06 15:59:53 -0800

    Update random_split.py

--- scripts/random_split.py
+++ scripts/random_split.py
@@ -4,7 +4,8 @@ import pandas as pd
 import numpy as np
 input_csv = "reddit_comments.csv"
 output_csv = "reddit_sample.csv"
-df = pd.read_csv(input_csv, parse_dates=["created_utc"])
+df = pd.read_csv(input_csv,low_memory=False)
+df["created_utc"] = pd.to_datetime(df["created_utc"], errors="coerce")
 df["year"] = df["created_utc"].dt.year
 sampled_df = df.groupby("year", group_keys=False).apply(
     lambda x: x.sample(frac=0.3, random_state=6657)
