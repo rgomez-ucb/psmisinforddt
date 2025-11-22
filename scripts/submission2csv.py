fe1286c965f4b821e2b6d4e1cef47ad6609e2630
llllllllllIIIIl <jiyangli123@gmail.com>
2025-11-22 14:43:39 -0800

    Create submission2csv.py

---
+++ scripts/submission2csv.py
@@ -0,0 +1,23 @@
+import pandas as pd
+# CHANGE FILEPATH
+jsonl_file = "./data/PoliticalDiscussion_cleaned_submissions.jsonl"
+csv_file = "./data/reddit_submissions.csv"
+
+
+chunksize = 100000
+
+
+reader = pd.read_json(jsonl_file, lines=True, chunksize=chunksize)
+
+first_chunk = True
+for chunk in reader:
+
+    chunk["created_utc"] = pd.to_datetime(chunk["created_utc"], unit="s")
+
+
+    chunk = chunk[["created_utc", "ups", "selftext","id","title","downs"]]
+
+
+    chunk.to_csv(csv_file, mode='a', index=False, header=first_chunk)
+    first_chunk = False
+print("finished")
